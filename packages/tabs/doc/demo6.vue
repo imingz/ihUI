cf05deae209380dddc9ee432c09b07f5921b2dee
mingz-wang <mingz.wang@foxmail.com>
2023-06-27 20:47:20 +0800

    fix: 修复 tabs 的 demo6 选项卡无法正常关闭的 bug

--- packages/tabs/doc/demo6.vue
+++ packages/tabs/doc/demo6.vue
@@ -1,9 +1,58 @@
 <!-- html -->
 <template>
-  <i-tabs activeName="tow">
-    <i-tab-pane label="选项卡一" name="one" disabled> 选项卡一 </i-tab-pane>
-    <i-tab-pane label="选项卡二" name="tow" closable> 选项卡二 </i-tab-pane>
-    <i-tab-pane label="选项卡三" name="three"> 选项卡三 </i-tab-pane>
+  <i-tabs v-model="activeName" @onTabRemove="handleRemove">
+    <i-tab-pane
+      v-for="tab in TabList"
+      :key="tab.name"
+      :label="tab.title"
+      :name="tab.name"
+      :disabled="tab.disabled"
+      :closable="tab.closable"
+    >
+      {{ tab.content }}
+    </i-tab-pane>
   </i-tabs>
 </template>
 
+<!-- js -->
+<script setup>
+import { ref } from "vue";
+
+const activeName = ref("2");
+const TabList = ref([
+  {
+    title: "Tab 1",
+    name: "1",
+    content: "Tab 1 content",
+    disabled: true,
+  },
+  {
+    title: "Tab 2",
+    name: "2",
+    content: "Tab 2 is closable",
+    closable: true,
+  },
+  {
+    title: "Tab 3",
+    name: "3",
+    content: "Tab 3 content",
+  },
+]);
+
+const handleRemove = (tabName) => {
+  const tabs = TabList.value;
+  let activeTab = activeName.value;
+  if (activeTab === tabName) {
+    tabs.forEach((tab, index) => {
+      if (tab.name === tabName) {
+        const nextTab = tabs[index + 1] || tabs[index - 1];
+        if (nextTab) {
+          activeTab = nextTab.name;
+        }
+      }
+    });
+  }
+  activeName.value = activeTab;
+  TabList.value = tabs.filter((tab) => tab.name !== tabName);
+};
+</script>
